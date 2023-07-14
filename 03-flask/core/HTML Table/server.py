5932c5b875555348c1782f3e4665c1de997cc490
hazem juini <hazemjuini243@gmail.com>
2023-07-14 12:33:50 +0100

    Html Table

---
+++ 03-flask/core/HTML Table/server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    data = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi', 'full_name' : 'Michael Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin', 'full_name' : 'John Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen', 'full_name' : 'Mark Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel', 'full_name' : 'KB Tonel'}
+]
+
+    return render_template('index.html', data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
